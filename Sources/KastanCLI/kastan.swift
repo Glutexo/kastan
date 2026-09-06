f428a34d79b1be467d9aa940cb61874c86753c56
Glutexo <glutexo@icloud.com>
2026-09-06 22:21:14 +0200

    Prepare Kaštan 0.8.1

--- Sources/KastanCLI/kastan.swift
+++ Sources/KastanCLI/kastan.swift
@@ -90,7 +90,7 @@ struct KastanApp {
 
 /// Executes one Kaštan CLI invocation and renders its human-readable output in the selected language.
 struct CommandRunner {
-    let version = "0.8.0"
+    let version = "0.8.1"
     let dataSource: any TransitDataSource
     let dataSourceRegistry: TransitDataSourceRegistry
     let aliasFile: StopAliasFile
